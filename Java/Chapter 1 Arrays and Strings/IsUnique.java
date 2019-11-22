9c06263c1b86403bb0beb2a18c0cf997b12184c9
Nick Casey <nicholas.casey@outlook.com>
2019-11-22 17:59:00 -0500

    Initial commit

---
+++ Java/Chapter 1 Arrays and Strings/IsUnique.java
@@ -0,0 +1,14 @@
+public class IsUnique {
+    /* 
+        Problem: Implement an algorithm to determine if a string has all unique characters. 
+        What if you cannot use additional data structures? 
+    */
+
+    public Boolean isUnique (String str) {
+        return false; 
+    }
+
+    public static void main (String[] args) {
+        System.out.println(isUnique("hello")); 
+    }   
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+** CRACKING THE CODING INTERVIEW ** 
+
+In this repo, I'll be working through each of the questions from Gayle McDowell's Cracking the Coding Interview!
+
+I'll organise the answers first by language, and then by chapter > question.  
